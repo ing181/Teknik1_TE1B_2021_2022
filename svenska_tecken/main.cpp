8d459992eba4fcd0830c8ace5b27dbfa3f4d3a65
ing181 <ingcei0123@edu.lerum.se>
2022-03-06 10:49:02 +0100

    comments

--- svenska_tecken/main.cpp
+++ svenska_tecken/main.cpp
@@ -7,8 +7,11 @@ int main()
     // För att svenska tecken ska fungera
     // i Windows. Ändrar teckenkodningen till 1252
     // där svenska tecken finns med
+    // CHCP "Change codepage" till 1252
+    // system betyder att vi startar ett Windowsprogram, i detta fall "CHCP"
     system("CHCP 1252");
-    // Suddar ut det som skrivits ut "Clear Screen"
+    // Här startar vi programmet "cls"
+    // som suddar ut det som skrivits ut "Clear Screen"
     system("CLS");
     cout << "ÅÄÖ åäö" << endl;
     return 0;
